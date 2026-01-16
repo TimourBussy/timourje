a3db384a99fb9927ed17445b115b809e7b9e5218
TimourBussy <timourjbanov@gmail.com>
2026-01-16 13:24:11 +0100

    Portfolio design finished

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -51,7 +51,12 @@ export function ProjectCard({
 								from-red-500 to-pink-500
 								dark:from-cyan-500 dark:to-purple-500"
 						/>
-						<span className="text-sm font-medium text-gray-600 dark:text-gray-400">
+						<span
+							className="
+								text-sm
+								text-red-600
+								dark:text-cyan-300"
+						>
 							{lang === "FR" ? "Projet " : "Project "} #{id}
 						</span>
 					</div>
@@ -73,10 +78,22 @@ export function ProjectCard({
 						target="_blank"
 						rel="noopener noreferrer"
 						className="
-							px-6 py-2 rounded-lg text-white font-medium self-start inline-block text-center bg-gradient-to-r transition-opacity hover:opacity-90
+							px-6 py-3 rounded-lg text-white font-medium flex items-center self-center gap-2.5 text-center bg-gradient-to-r transition-opacity hover:opacity-90
 							from-red-500 to-pink-500
 							dark:from-cyan-500 dark:to-purple-500"
 					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 256 256"
+							fill="currentColor"
+							className="w-3.5 h-3.5 translate-y-0.25"
+						>
+							<g transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
+								<path d="M 84 37.02 c -3.313 0 -6 -2.687 -6 -6 V 12 H 58.98 c -3.313 0 -6 -2.687 -6 -6 s 2.687 -6 6 -6 H 84 c 3.313 0 6 2.687 6 6 v 25.02 C 90 34.333 87.313 37.02 84 37.02 z" />
+								<path d="M 33.961 62.039 c -1.536 0 -3.071 -0.586 -4.243 -1.758 c -2.343 -2.343 -2.343 -6.142 0 -8.484 l 50.039 -50.04 c 2.342 -2.343 6.143 -2.343 8.484 0 c 2.344 2.343 2.344 6.142 0 8.485 L 38.204 60.281 C 37.033 61.453 35.497 62.039 33.961 62.039 z" />
+								<path d="M 73.658 90 H 16.341 C 7.331 90 0 82.669 0 73.658 V 16.341 C 0 7.331 7.331 0 16.341 0 h 17.62 c 3.313 0 6 2.687 6 6 s -2.687 6 -6 6 h -17.62 C 13.947 12 12 13.947 12 16.341 v 57.317 C 12 76.053 13.947 78 16.341 78 h 57.317 C 76.053 78 78 76.053 78 73.658 V 56.039 c 0 -3.313 2.687 -6 6 -6 s 6 2.687 6 6 v 17.619 C 90 82.669 82.669 90 73.658 90 z" />
+							</g>
+						</svg>
 						{lang === "FR" ? "Voir le projet" : "View Project"}
 					</a>
 				</div>
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,18 @@
 import { Heading } from "../components/ui/Heading";
+import { Paragraph } from "../components/ui/Paragraph";
 
 export function Contact({ lang }: { lang: "FR" | "EN" }) {
 	return (
 		<>
-			<Heading level={3} variant="soft" className="mt-7">
-				CV
+			<Heading level={3} variant="soft">
+				Contact
 			</Heading>
+
+			<Paragraph size="big" className="mt-4">
+				{lang === "FR"
+					? "N'hésitez pas à me contacter !"
+					: "Feel free to reach out to me!"}
+			</Paragraph>
 		</>
 	);
 }
